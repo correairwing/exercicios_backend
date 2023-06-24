bab552cda89118f5f57840312495bd76d90aea64
Irwing Correa <126804430+correairwing@users.noreply.github.com>
2023-06-24 14:38:12 -0300

    commit inicial

---
+++ TarefaCalculoMedia/src/br/com/icorrea/Medias.java
@@ -0,0 +1,20 @@
+package br.com.icorrea;
+
+public class Medias {
+
+	public static void main(String[] args) {
+		int nota1 = 7;
+		int nota2 = 8;
+		int nota3 = 8;
+		int nota4 = 10;
+		System.out.println("Nota 1: " + nota1);
+		System.out.println("Nota 2: " + nota2);
+		System.out.println("Nota 3: " + nota3);
+		System.out.println("Nota 4: " + nota4);
+		System.out.println("  Media final: " + (nota1 + nota2 + nota3 + nota4) /4);
+				
+	}
+
+	
+
+}
