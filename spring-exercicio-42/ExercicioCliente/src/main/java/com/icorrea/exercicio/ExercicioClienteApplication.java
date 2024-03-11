0f81aa432556d4f2bba6d1fd3f5a95fc7c22ee3d
Irwing <irwingcorrea95@gmail.com>
2024-03-11 12:12:29 -0300

    initial commit

---
+++ spring-exercicio-42/ExercicioCliente/src/main/java/com/icorrea/exercicio/ExercicioClienteApplication.java
@@ -0,0 +1,13 @@
+package com.icorrea.exercicio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExercicioClienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExercicioClienteApplication.class, args);
+	}
+
+}
---
+++ spring-exercicio-42/ExercicioProduto/src/main/java/com/icorrea/exercicio/ExercicioProdutoApplication.java
@@ -0,0 +1,13 @@
+package com.icorrea.exercicio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExercicioProdutoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExercicioProdutoApplication.class, args);
+	}
+
+}
